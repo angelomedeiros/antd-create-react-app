f4bba28961706a6a1fb67e36971262e5726fbdbf
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-10-02 16:43:40 -0300

    Refatorando o componente table:
    - Definindo a largura das colunas;
    - Removendo o fetch para o componente principal;
    - Add o loading

--- src/home/table.js
+++ src/home/table.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Table, Divider, Tag } from 'antd';
-import axios from 'axios'
+import { Table } from 'antd';
 
 const columns = [{
   title: 'Issue Number',
   dataIndex: 'number',
-  width: '10%'
+  width: '10%',
 }, {
   title: 'Title',
   dataIndex: 'title',
@@ -14,12 +13,12 @@ const columns = [{
   title: 'Created At',
   dataIndex: 'created_at',
   width: '15%',
-  render: created_at => `${new Date(created_at).toDateString()}`
+  render: created_at => created_at ? `${new Date(created_at).toDateString()}` : ''
 }, {
   title: 'Updated At',
   dataIndex: 'updated_at',
   width: '15%',
-  render: updated_at => `${new Date(updated_at).toDateString()}`
+  render: updated_at => updated_at ? `${new Date(updated_at).toDateString()}` : ''
 }, {
   title: 'Labels',
   dataIndex: 'labels',
@@ -27,10 +26,9 @@ const columns = [{
   render: labels => Array.isArray(labels) ?
     labels.map((label) => (
       <span key={label.id}>
-        {label && label.name || 'No label'}
+        {(label && label.name) || ''}
       </span>
-    )) :
-    <span>No label</span>
+    )) : ''
 }, {
   title: 'State',
   dataIndex: 'state',
@@ -38,23 +36,6 @@ const columns = [{
 }];
 
 
-export default class TableMy extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      dataRepo: [{}]
-    }
-  }
-
-  componentDidMount() {
-    axios.get('https://api.github.com/repos/facebook/react/issues').then(
-      response => this.setState({ dataRepo: response.data })
-    )
-  }
-
-  render() {
-    return (
-      <Table columns={columns} dataSource={this.state.dataRepo} rowKey={data => data.id} />
-    )
-  }
-}
+export default props => (
+  <Table columns={columns} loading={props.loading} dataSource={props.dataSource} rowKey={data => data.id} />
+)
